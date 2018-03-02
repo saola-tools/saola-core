57ae1e5513d72049ec9760adbf3f18f17d07196d
pnhung177 <pnhung177@acegik.net>
2018-03-02 15:45:42 +0700

    Remove NODE_ prefix in environment variable names

--- test/cmd/bdd.sh
+++ test/cmd/bdd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find $(dirname $0)/../bdd -name '*-test.js' | xargs ./node_modules/.bin/mocha -R spec;
+npm run build && find $(dirname $0)/../bdd -name '*-test.js' | xargs node_modules/.bin/mocha -R spec;
 true
--- test/cmd/tdd.sh
+++ test/cmd/tdd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-node_modules/.bin/mocha --recursive test/tdd/**/*-test.js;
+npm run build && node_modules/.bin/mocha --recursive test/tdd/**/*-test.js;
 true
