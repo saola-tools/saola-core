e56b73c8d54ecb05d028290c4b5b2fa22ee8e276
pnhung177 <pnhung177@acegik.net>
2018-03-03 07:45:46 +0700

    Refactor config-loader source code

--- test/cmd/bdd.sh
+++ test/cmd/bdd.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-npm run build && find $(dirname $0)/../bdd -name '*-test.js' | xargs node_modules/.bin/mocha -R spec;
+npm run build && node_modules/.bin/mocha --recursive test/bdd/**/*-test.js;
+#npm run build && find $(dirname $0)/../bdd -name '*-test.js' | xargs node_modules/.bin/mocha -R spec;
 true
