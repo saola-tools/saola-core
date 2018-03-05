9a375965ed1f9418e756c0e35db3fb64f19af7c1
pnhung177 <pnhung177@acegik.net>
2018-03-05 13:03:41 +0700

    Add nested and overlap plugins testcase for expandExtensions()

--- test/tdd/bootstrap-test.js
+++ test/tdd/bootstrap-test.js
@@ -236,6 +236,54 @@ describe('tdd:devebot:base:bootstrap', function() {
       });
     });
 
+    it('expand empty context with nested and overlap plugins', function() {
+      var output = expandExtensions(null, [
+        {
+          name: 'sublib1',
+          path: lab.getLibHome('sublib1')
+        },
+        {
+          name: 'sublib2',
+          path: lab.getLibHome('sublib2')
+        }
+      ]);
+      assert.isArray(output.libRootPaths);
+      output.libRootPaths = lodash.map(output.libRootPaths, replaceLibPath);
+      assert.isObject(output.pluginRefs);
+      output.pluginRefs = lodash.mapValues(output.pluginRefs, function(v) {
+        v.path = replaceLibPath(v.path);
+        return v;
+      });
+      assert.isObject(output.bridgeRefs);
+      output.bridgeRefs = lodash.mapValues(output.bridgeRefs, function(v) {
+        v.path = replaceLibPath(v.path);
+        return v;
+      });
+      false && console.log('expandExtensions(): ', output);
+      assert.deepEqual(output, {
+        libRootPaths:
+          ['/test/lib/sublib1',
+            '/test/lib/plugin1',
+            '/test/lib/plugin2',
+            '/test/lib/sublib2',
+            '/test/lib/plugin3'],
+        pluginRefs:
+          {
+            sublib1: { name: 'sublib1', path: '/test/lib/sublib1/index.js' },
+            sublib2: { name: 'sublib2', path: '/test/lib/sublib2/index.js' },
+            plugin1: { name: 'plugin1', path: '/test/lib/plugin1/index.js' },
+            plugin2: { name: 'plugin2', path: '/test/lib/plugin2/index.js' },
+            plugin3: { name: 'plugin3', path: '/test/lib/plugin3/index.js' }
+          },
+        bridgeRefs:
+          {
+            bridge1: { name: 'bridge1', path: '/test/lib/bridge1/index.js' },
+            bridge2: { name: 'bridge2', path: '/test/lib/bridge2/index.js' },
+            bridge3: { name: 'bridge3', path: '/test/lib/bridge3/index.js' }
+          }
+      });
+    });
+
     it('expand empty context with complete plugins (nested and overlap plugins)', function() {
       var output = expandExtensions(null, [
         {
