84f52b8485d82d319e67d3e275a23886b80edebe
pnhung177 <pnhung177@acegik.net>
2018-03-10 17:22:43 +0700

    test old logging messages are mapped to new logging labels

--- test/tdd/logging-factory-test.js
+++ test/tdd/logging-factory-test.js
@@ -10,6 +10,8 @@ var expect = require('chai').expect;
 var path = require('path');
 var util = require('util');
 var LoggingFactory = require('../../lib/backbone/logging-factory');
+var LogAdapter = require('logolite').LogAdapter;
+var MockLogger = require('logolite').MockLogger;
 var envtool = require('logolite/envtool');
 var rewire = require('rewire');
 
@@ -20,6 +22,16 @@ describe('tdd:devebot:core:logging-factory', function() {
     var transformLoggingLabels = LoggingFactory.__get__('transformLoggingLabels');
 		assert.isNotNull(transformLoggingLabels);
 		
+		before(function() {
+			envtool.setup({
+				LOGOLITE_ALWAYS_MUTED: 'all'
+			});
+		});
+
+		beforeEach(function() {
+			LoggingFactory.reset();
+		});
+
 		it('transformLoggingLabels() accept null parameter', function() {
 			var output = transformLoggingLabels(null);
 			false && console.log('transformLoggingLabels(): ', output);
@@ -106,15 +118,87 @@ describe('tdd:devebot:core:logging-factory', function() {
 			assert.deepEqual(output, expected);
 		});
 
-		it('old logging messages are mapped to new logging labels');
+		it('old logging messages are mapped to new logging labels', function() {
+			var factory = new LoggingFactory({
+				profileConfig: {
+					logger: {
+						labels: {
+							level_s: {
+								level: 5,
+								color: 'magenta',
+								link: 'silly'
+							},
+							level_d: {
+								level: 4,
+								color: 'blue',
+								link: 'debug'
+							},
+							level_i: {
+								level: 3,
+								color: 'cyan',
+								link: 'info'
+							},
+							level_t: {
+								level: 2,
+								color: 'green',
+								link: 'trace'
+							},
+							level_w: {
+								level: 1,
+								color: 'yellow',
+								link: 'warn'
+							},
+							level_e: {
+								level: 0,
+								color: 'red',
+								link: 'error'
+							}
+						},
+						transports: {
+							console: {
+								type: 'console',
+								level: 'level_d',
+								json: false,
+								timestamp: true,
+								colorize: true
+							}
+						}
+					}
+				}
+			});
+
+			var rootTracer = factory.getTracer();
+			var rootLogger = factory.getLogger();
+			var mockLogger = new MockLogger({
+				levels: {
+					level_s: 5,
+					level_d: 4,
+					level_i: 3,
+					level_t: 2,
+					level_w: 1,
+					level_e: 0
+				}
+			});
+			LogAdapter.addInterceptor(mockLogger);
+
+			rootLogger.log('error', 'Error message #1');
+			rootLogger.log('error', 'Error message #2');
+			rootLogger.log('debug', 'Debug message #1');
+
+			var msgs = mockLogger._reset();
+			false && console.log(JSON.stringify(msgs, null, 2));
+    });
+
+		after(function() {
+			envtool.reset();
+		});
 	});
 
 	describe('extend Tracer using branch() method', function() {
 
 		before(function() {
 			envtool.setup({
-				LOGOLITE_DEBUGLOG_ENABLED: null,
-				LOGOLITE_MOCKLOGGER_ENABLED: 'true'
+				LOGOLITE_ALWAYS_MUTED: 'all'
 			});
 		});
 
@@ -123,6 +207,9 @@ describe('tdd:devebot:core:logging-factory', function() {
 		});
 
 		it('default Tracer must contain framework information', function() {
+			var mockLogger = new MockLogger();
+			LogAdapter.clearInterceptors().addInterceptor(mockLogger);
+
 			var factory = new LoggingFactory({
 				logger: {
 					transports: {
@@ -139,9 +226,7 @@ describe('tdd:devebot:core:logging-factory', function() {
 
 			var rootTracer = factory.getTracer();
 
-			var mockLogger = factory.getLogger({ type: 'shadow' });
-			var queue = mockLogger._probe();
-			mockLogger._reset();
+			var queue = mockLogger._reset();
 
 			assert.equal(queue.length, 2);
 			queue.forEach(function(item) {
@@ -157,6 +242,9 @@ describe('tdd:devebot:core:logging-factory', function() {
 		});
 
 		it('recursive branch() calls will return hierarchical loggingFactory objects', function() {
+			var mockLogger = new MockLogger();
+			LogAdapter.clearInterceptors().addInterceptor(mockLogger);
+
 			var factory = new LoggingFactory({
 				logger: {
 					transports: {
@@ -178,11 +266,9 @@ describe('tdd:devebot:core:logging-factory', function() {
 
 			var logObject_1 = childFactory1.getTracer().toMessage();
 
-			var mockLogger = factory.getLogger({ type: 'shadow' });
-			var queue = mockLogger._probe();
-			mockLogger._reset();
+			var queue = mockLogger._reset();
 
-			assert.equal(queue.length, 5);
+			assert.equal(queue.length, 2 + 4);
 			queue.forEach(function(item) {
 				item.payload = JSON.parse(item.payload);
 			});
@@ -199,20 +285,20 @@ describe('tdd:devebot:core:logging-factory', function() {
 			assert.isTrue(factory.getTracer() !== factory_2_1.getTracer());
 			assert.isTrue(factory.getLogger() !== factory_2_1.getLogger());
 
-			assert.equal(lodash.get(queue, [0, 'payload', 'blockName']), 'devebot');
-			assert.equal(lodash.get(queue, [1, 'payload', 'blockName']), 'child1');
-			assert.equal(lodash.get(queue, [2, 'payload', 'blockName']), 'child2');
-			assert.equal(lodash.get(queue, [3, 'payload', 'blockName']), 'grand-child-1');
-			assert.equal(lodash.get(queue, [4, 'payload', 'blockName']), 'grand-child-2');
+			assert.equal(lodash.get(queue, [1, 'payload', 'blockName']), 'devebot');
+			assert.equal(lodash.get(queue, [2, 'payload', 'blockName']), 'child1');
+			assert.equal(lodash.get(queue, [3, 'payload', 'blockName']), 'child2');
+			assert.equal(lodash.get(queue, [4, 'payload', 'blockName']), 'grand-child-1');
+			assert.equal(lodash.get(queue, [5, 'payload', 'blockName']), 'grand-child-2');
 
 			assert.deepEqual(
 				lodash.pick(JSON.parse(logObject_1), ['instanceId', 'blockId']),
-				lodash.pick(lodash.get(queue, [1, 'payload']), ['instanceId', 'blockId'])
+				lodash.pick(lodash.get(queue, [2, 'payload']), ['instanceId', 'blockId'])
 			);
 
 			assert.deepEqual(
 				lodash.pick(JSON.parse(logObject_2), ['instanceId', 'blockId']),
-				lodash.pick(lodash.get(queue, [2, 'payload']), ['instanceId', 'blockId'])
+				lodash.pick(lodash.get(queue, [3, 'payload']), ['instanceId', 'blockId'])
 			);
 		});
 
