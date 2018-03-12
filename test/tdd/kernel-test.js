0b23e147b786391c60d834e01582cf849365862a
pnhung177 <pnhung177@acegik.net>
2018-03-12 13:34:06 +0700

    Remove console.log() out of kernel.js

--- test/tdd/kernel-test.js
+++ test/tdd/kernel-test.js
@@ -36,7 +36,13 @@ describe('tdd:devebot:base:kernel', function() {
         accumulator: loggingStore,
         mappings: [{
           allTags: [ 'devebot-kernel', 'loadSchemas' ],
-          storeTo: 'schemaCollection'
+          storeTo: 'schemaValidation'
+        }, {
+          allTags: [ 'devebot-kernel', 'config-schema-synchronizing' ],
+          storeTo: 'schemaValidation'
+        }, {
+          allTags: [ 'devebot-kernel', 'config-schema-validating' ],
+          storeTo: 'schemaValidation'
         }]
       }]);
     });
@@ -47,10 +53,168 @@ describe('tdd:devebot:base:kernel', function() {
 
     it('load all of schemas from kernel constructor', function() {
       var kernel = lab.createKernel('fullapp');
-      var configMap = lodash.get(loggingStore, 'schemaCollection.0.configMap', {});
-      var schemaMap = lodash.get(loggingStore, 'schemaCollection.0.schemaMap', {});
+
+      var configMap = lodash.get(loggingStore, 'schemaValidation.0.configMap', {});
+      var schemaMap = lodash.get(loggingStore, 'schemaValidation.0.schemaMap', {});
       false && console.log('Config: %s', JSON.stringify(configMap, null, 2));
       false && console.log('Schema: %s', JSON.stringify(schemaMap, null, 2));
+
+      var configObject = lodash.get(loggingStore, 'schemaValidation.1.configObject', {});
+      var configSchema = lodash.get(loggingStore, 'schemaValidation.1.configSchema', {});
+      false && console.log('configObject: %s', JSON.stringify(configObject, null, 2));
+      false && console.log('configSchema: %s', JSON.stringify(configSchema, null, 2));
+
+      var result = lodash.get(loggingStore, 'schemaValidation.2.validatingResult', []);
+      true && lodash.forEach(result, function(item) {
+        console.log('- validation result: %s', JSON.stringify(lodash.omit(item, ['stack'])));
+        if (item.hasError) {
+          console.log('  - stack:\n%s', item.stack);
+        }
+      });
+
+      assert.deepEqual(configObject.sandbox, {
+        "application": {
+          "host": "0.0.0.0",
+          "port": 17700,
+          "verbose": false
+        },
+        "bridges": {
+          "anyname1a": {
+            "bridge1": {
+              "refPath": "sandbox -> bridge1 -> anyname1a"
+            }
+          },
+          "anyname1b": {
+            "bridge1": {
+              "refPath": "sandbox -> bridge1 -> anyname1b"
+            }
+          },
+          "anyname2a": {
+            "bridge2": {
+              "refPath": "sandbox -> bridge2 -> anyname2a"
+            }
+          },
+          "anyname2b": {
+            "bridge2": {
+              "refPath": "sandbox -> bridge2 -> anyname2b"
+            }
+          },
+          "anyname2c": {
+            "bridge2": {
+              "refPath": "sandbox -> bridge2 -> anyname2c"
+            }
+          }
+        },
+        "plugins": {
+          "plugin1": {
+            "host": "0.0.0.0",
+            "port": 17701,
+            "verbose": false,
+            "tdd": {
+              "field1": "String 1",
+              "field2": 10001,
+              "field3": {
+                "foo": "foo",
+                "bar": "bar",
+                "num": 1001
+              },
+              "field4": [
+                1,
+                2,
+                3,
+                null,
+                "4"
+              ]
+            }
+          },
+          "plugin2": {
+            "host": "0.0.0.0",
+            "port": 17702,
+            "verbose": false,
+            "tdd": {
+              "field1": "String 2",
+              "field2": 10002,
+              "field3": {
+                "foo": "foo",
+                "bar": "bar",
+                "num": 1002
+              },
+              "field4": [
+                1,
+                2,
+                3,
+                null,
+                "4"
+              ]
+            }
+          },
+          "subPlugin1": {
+            "host": "localhost",
+            "port": 17721
+          },
+          "subPlugin2": {
+            "host": "localhost",
+            "port": 17722
+          },
+          "plugin3": {
+            "host": "localhost",
+            "port": 17703
+          }
+        }
+      });
+      assert.deepEqual(configSchema.sandbox, {
+        "application": {
+          "moduleId": "fullapp",
+          "schema": {
+            "type": "object",
+            "properties": {
+              "host": {
+                "type": "string"
+              },
+              "port": {
+                "type": "number"
+              },
+              "verbose": {
+                "type": "boolean"
+              }
+            },
+            "required": [
+              "host",
+              "port"
+            ]
+          }
+        },
+        "plugins": {
+          "subPlugin1": {
+            "moduleId": "sub-plugin1",
+            "schema": {
+              "type": "object",
+              "properties": {
+                "host": {
+                  "type": "string"
+                },
+                "port": {
+                  "type": "number"
+                }
+              }
+            }
+          },
+          "subPlugin2": {
+            "moduleId": "sub-plugin2",
+            "schema": {
+              "type": "object",
+              "properties": {
+                "host": {
+                  "type": "string"
+                },
+                "port": {
+                  "type": "number"
+                }
+              }
+            }
+          }
+        }
+      });
     });
 
     after(function() {
