ae6cea243f32ef006033aa2d96ca96246a330dc7
pnhung177 <pnhung177@acegik.net>
2018-05-29 10:27:12 +0700

    Add DEVEBOT_VERIFICATION_MODE to verify config

---
+++ test/app/state-verification/config/profile.js
@@ -0,0 +1,37 @@
+module.exports = {
+  devebot: {
+    verbose: true,
+    jobqueue: {
+      enabled: true
+    }
+  },
+  logger: {
+    transports: {
+      console: {
+        type: 'console',
+        level: 'error',
+        json: false,
+        timestamp: true,
+        colorize: true
+      }
+    }
+  },
+  newFeatures: {
+    application: {
+      logoliteEnabled: true,
+      sandboxConfig: true
+    },
+    bridgeKebabCase1: {
+      logoliteEnabled: true
+    },
+    bridgeKebabCase2: {
+      logoliteEnabled: true
+    },
+    connector1: {
+      logoliteEnabled: true
+    },
+    connector2: {
+      logoliteEnabled: true
+    }
+  }
+}
---
+++ test/app/state-verification/lib/services/main-service.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var Promise = Devebot.require('bluebird');
+var chores = Devebot.require('chores');
+var lodash = Devebot.require('lodash');
+var debugx = Devebot.require('pinbug')('devebot:test:lab:main:mainService');
+
+var Service = function(params) {
+  params = params || {};
+  var self = this;
+
+  var LX = params.loggingFactory.getLogger();
+  var LT = params.loggingFactory.getTracer();
+
+  debugx.enabled && debugx(' + constructor begin ...');
+
+  var mainCfg = lodash.get(params, ['sandboxConfig'], {});
+  debugx.enabled && debugx('configuration: %s', JSON.stringify(mainCfg));
+
+  self.getConfig = function() {
+    return lodash.cloneDeep(mainCfg);
+  }
+
+  debugx.enabled && debugx(' - constructor end!');
+};
+
+if (chores.isFeatureSupported('bridge-full-ref')) {
+  Service.referenceList = [
+    'devebot-dp-wrapper1/sublibService',
+    'devebot-dp-wrapper2/sublibService'
+  ]
+}
+
+module.exports = Service;
