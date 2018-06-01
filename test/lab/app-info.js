e57abe3bf8f72ca101af3818650055d01b85510e
pnhung177 <pnhung177@acegik.net>
2018-06-01 15:19:38 +0700

    Fixbug in app-info.js script

--- test/lab/app-info.js
+++ test/lab/app-info.js
@@ -1,11 +1,13 @@
 'use strict';
 
+var path = require('path');
 var argv = require('minimist')(process.argv.slice(2));
+console.log('Command arguments: %s', JSON.stringify(argv, null, 2));
 
-var appName = argv['app'] || argv['a'] || 'simple';
+var appName = argv['app'] || argv['a'] || 'default';
 
-var main = require('./index').getApp(appName);
+var main = require(path.join(__dirname, '../index')).getApp(appName);
 main.runner.invoke(function(injektor) {
-	console.log('profile config: %s', JSON.stringify(injektor.lookup('profileConfig'), null, 2));
+	console.log('Profile config: %s', JSON.stringify(injektor.lookup('profileConfig'), null, 2));
 	return Promise.resolve();
 });
