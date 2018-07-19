3d333883b4b651033641b3173e55ae9e804e602c
pnhung177 <pnhung177@acegik.net>
2018-07-19 15:18:24 +0700

    Refactoring pmtool utility

--- src/utils/pmtool.js
+++ src/utils/pmtool.js
@@ -1,13 +1,26 @@
 'use strict';
 
-const DEFAULT_SCOPE = require('./getenv')('DEVEBOT_DEFAULT_SCOPE', 'devebot');
+const getenv = require('./getenv');
+const DEFAULT_SCOPE = getenv('DEVEBOT_DEFAULT_SCOPE', 'devebot');
 const debugx = require('./pinbug')(DEFAULT_SCOPE + ':utils:pmtool');
 
-function PmTool() {
+function PmTool(params) {
+  params = params || {};
+
+  let {config, errorCollector} = params;
+
   let pm_id = parseInt(process.env.pm_id);
   let pm_total = parseInt(process.env.instances);
 
   debugx.enabled && debugx('PM2 environment: id: %s / total: %s', pm_id, pm_total);
+
+  Object.defineProperty(this, 'available', {
+    get: function() {
+      return typeof(pm_id) === 'number' && !isNaN(pm_id) &&
+          typeof(pm_total) === 'number' && !isNaN(pm_total);
+    },
+    set: function(value) {}
+  });
 
   Object.defineProperty(this, 'id', {
     get: function() {
@@ -23,16 +36,8 @@ function PmTool() {
     set: function(value) {}
   });
 
-  Object.defineProperty(this, 'enabled', {
-    get: function() {
-      return typeof(pm_id) === 'number' && !isNaN(pm_id) &&
-          typeof(pm_total) === 'number' && !isNaN(pm_total);
-    },
-    set: function(value) {}
-  });
-
   this.belongTo = function(idx) {
-    if (!this.enabled || pm_id < 0 || pm_total <= pm_id) return null;
+    if (!this.available || pm_id < 0 || pm_total <= pm_id) return null;
     while(idx >= pm_total) idx -= pm_total;
     return idx === pm_id;
   }
