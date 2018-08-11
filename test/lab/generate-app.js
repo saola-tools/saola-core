69e5ecdc4a0b614c06895ca954feb8e6b8c2e48c
pnhung177 <pnhung177@acegik.net>
2018-08-11 17:09:05 +0700

    Define AppGenerator class

--- test/lab/generate-app.js
+++ test/lab/generate-app.js
@@ -1,41 +1,45 @@
 
 var path = require('path');
-var util = require('util');
 var Temporify = require('temporify');
 
-function buildApp(params) {
+function AppGenerator(params) {
   params = params || {};
 
   var projectName = params.projectName || 'example-project';
-  var builder = new Temporify({
-    subdir: projectName
-  });
-  let basedir = builder.basedir;
+  var testHomeDir = params.testHomeDir || path.join(__dirname, '../index');
 
-  builder.add([{
+  var builder = new Temporify({
+    subdir: projectName,
+    variables: {
+      projectName: projectName,
+      testHomeDir: testHomeDir
+    }
+  });
+
+  builder.assign([{
     filename: 'index.js',
-    content: `
+    template: `
     'use strict';
     var lab = require('<%- testHomeDir %>');
     var Devebot = lab.getDevebot();
     var app = Devebot.launchApplication({
-      appRootPath: __dirname
+      appRootPath: __dirname,
+      <% if (privateProfile) { %>
+      privateProfile: <%- privateProfile %>
+      <% } %>
     });
     if (require.main === module) app.server.start();
     module.exports = app;
-    `,
-    model: {
-      testHomeDir: params.testHomeDir
-    }
+    `
   }, {
     dir: 'config/',
     filename: 'sandbox.js',
-    content: `
+    template: `
       module.exports = {};
     `
   }, {
     filename: 'package.json',
-    content: `
+    template: `
     {
       "name": "<%- projectName %>",
       "version": "0.1.0",
@@ -47,50 +51,31 @@ function buildApp(params) {
       "author": "devebot",
       "license": "ISC"
     }
-    `,
-    model: {
-      projectName: projectName
-    }
+    `
   }]);
 
-  return function(args) {
-    args = args || {};
-    var context = {
-      appRootPath: builder.homedir
-    }
-    if (args.privateProfile) {
-      context.privateProfile = args.privateProfile;
-    }
-    builder.add({
+  this.customize = function(args) {
+    return builder.assign({
       filename: 'index.js',
-      content: `
-      'use strict';
-      var lab = require('<%- testHomeDir %>');
-      var Devebot = lab.getDevebot();
-      var app = Devebot.launchApplication(<%- context %>);
-      if (require.main === module) app.server.start();
-      module.exports = app;
-      `,
-      model: {
-        testHomeDir: params.testHomeDir,
-        context: JSON.stringify(context)
-      }
-    });
-    builder.generate();
-    return builder;
+      variables: args
+    }).generate();
   };
+
+  this.destroy = function() {
+    builder.destroy();
+  }
 }
 
 var flow = Promise.resolve();
 
 flow = flow.then(function() {
-  var build = buildApp({
+  var generator = new AppGenerator({
     projectName: 'invalid-appbox-context',
     testHomeDir: path.join(__dirname, '../index')
   });
 
-  var tempor = build({
-    privateProfile: 'abc'
+  var tempor = generator.customize({
+    privateProfile: JSON.stringify('123')
   });
 
   console.log('Home: %s', tempor.homedir);
@@ -105,7 +90,7 @@ flow = flow.then(function() {
   });
 
   subf = subf.then(function() {
-    tempor.destroy();
+    generator.destroy();
   });
 
   return subf;
