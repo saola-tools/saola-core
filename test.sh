01b4d60df8eab58435d070b5103bd9ec36d4bdba
pnhung177 <pnhung177@acegik.net>
2018-11-03 10:36:55 +0700

    Add test-without-rebuilding target

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 npm test
-DEVEBOT_UPGRADE_DISABLED=presets npm test
-DEVEBOT_UPGRADE_DISABLED=bridge-full-ref npm test
-DEVEBOT_UPGRADE_DISABLED=bridge-full-ref,presets npm test
-DEVEBOT_UPGRADE_DISABLED=standardizing-config npm test
-DEVEBOT_UPGRADE_DISABLED=standardizing-config,bridge-full-ref npm test
-DEVEBOT_UPGRADE_DISABLED=standardizing-config,bridge-full-ref,presets npm test
-DEVEBOT_UPGRADE_DISABLED=gadget-around-log npm test
+DEVEBOT_UPGRADE_DISABLED=presets npm run test-without-rebuilding
+DEVEBOT_UPGRADE_DISABLED=bridge-full-ref npm run test-without-rebuilding
+DEVEBOT_UPGRADE_DISABLED=bridge-full-ref,presets npm run test-without-rebuilding
+DEVEBOT_UPGRADE_DISABLED=standardizing-config npm run test-without-rebuilding
+DEVEBOT_UPGRADE_DISABLED=standardizing-config,bridge-full-ref npm run test-without-rebuilding
+DEVEBOT_UPGRADE_DISABLED=standardizing-config,bridge-full-ref,presets npm run test-without-rebuilding
+DEVEBOT_UPGRADE_DISABLED=gadget-around-log npm run test-without-rebuilding
