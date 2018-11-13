de3f0d0f8ff3bd2000c6af935424b865fabf3f05
pnhung177 <pnhung177@acegik.net>
2018-11-13 13:45:50 +0700

    Support 3 method types: general, promise, callback for wrapMethod [object-decorator]

--- src/backbone/object-decorator.js
+++ src/backbone/object-decorator.js
@@ -1,48 +1,31 @@
 'use strict';
 
+const Promise = require('bluebird');
 const lodash = require('lodash');
 const path = require('path');
 const chores = require('../utils/chores');
 const constx = require('../utils/constx');
 const loader = require('../utils/loader');
+const nodash = require('../utils/nodash');
 const blockRef = chores.getBlockRef(__filename);
 
 function ObjectDecorator(params={}) {
-  let loggingFactory = params.loggingFactory.branch(blockRef);
-  let L = loggingFactory.getLogger();
-  let T = loggingFactory.getTracer();
+  const loggingFactory = params.loggingFactory.branch(blockRef);
+  const L = loggingFactory.getLogger();
+  const T = loggingFactory.getTracer();
+  const C = lodash.assign({L, T}, lodash.pick(params, ['issueInspector', 'schemaValidator']));
+  const bridgeCTX = lodash.assign({moduleType: 'bridge'}, C);
+  const pluginCTX = lodash.assign({moduleType: 'plugin'}, C);
 
-  this.wrapBean = function(bean, opts) {
-    if (!lodash.isObject(bean)) return bean;
-    opts = opts || {};
-    let self = this;
-    return self.wrapMethodsOf(bean, opts);
+  this.wrapBridgeDialect = function(bean, opts) {
+    return wrapObject(bridgeCTX, textureStore, bean, opts);
   }
 
-  this.wrapMethodsOf = function(object, opts) {
-    let self = this;
-    if (lodash.isObject(object)) {
-      opts = opts || {};
-      for(let name in object) {
-        if(lodash.isFunction(object[name])) {
-          object[name] = self.wrapMethod(object[name], {
-            object: object,
-            objectName: opts.objectName,
-            methodName: name
-          });
-        }
-      }
-    }
-    return object;
+  this.wrapPluginGadget = function(bean, opts) {
+    return wrapObject(pluginCTX, textureStore, bean, opts);
   }
 
-  this.wrapMethod = function(method, opts) {
-    if (!lodash.isFunction(method)) return method;
-    let object = opts && lodash.isObject(opts.object) && opts.object || null;
-    return function() {
-      return method.apply(object, arguments);
-    }
-  }
+  let textureStore = lodash.get(params, ['textureConfig']);
 }
 
 ObjectDecorator.argumentSchema = {
@@ -71,3 +54,153 @@ ObjectDecorator.argumentSchema = {
 };
 
 module.exports = ObjectDecorator;
+
+function wrapObject(ref, textureStore, object, opts) {
+  ref = ref || {};
+  if (lodash.isObject(object)) {
+    opts = opts || {};
+    for(let name in object) {
+      if(lodash.isFunction(object[name])) {
+        let texture = null;
+        if (ref.moduleType === 'bridge') {
+
+        }
+        if (ref.moduleType === 'plugin') {
+          texture = getTextureDef({
+            textureStore: textureStore,
+            pluginCode: opts.pluginCode,
+            gadgetType: opts.gadgetType,
+            objectName: opts.objectName,
+            methodName: name
+          });
+        }
+        object[name] = wrapMethod(ref, object[name], texture, {
+          object: object,
+          objectName: opts.objectName,
+          methodName: name
+        });
+      }
+    }
+  }
+  return object;
+}
+
+function wrapMethod(ref, method, texture, opts) {
+  if (!nodash.isFunction(method)) return method;
+  let {object, objectName, methodName} = opts || {};
+  object = nodash.isObject(object) ? object : null;
+  let wrapped = method;
+  if (isDecorated(texture)) {
+    wrapped = function() {
+      let parameters = arguments;
+      let requestId = null;
+      const L = opts.L || ref.L;
+      const T = opts.T || ref.T;
+      // pre-processing logging texture
+      if (isEnabled(texture.logging)) {
+        let onRequest = texture.logging.onRequest;
+        if (isEnabled(onRequest)) {
+          let logArgs = {objectName, methodName};
+          if (nodash.isFunction(onRequest.extractReqId)) {
+            logArgs.requestId = requestId = onRequest.extractReqId(parameters);
+          }
+          if (nodash.isFunction(onRequest.extractInfo)) {
+            logArgs.parameters = onRequest.extractInfo(parameters);
+          }
+          let tmpl = "#{objectName}.#{methodName} - #{parameters} - #{requestId}";
+          if (nodash.isString(onRequest.template)) {
+            tmpl = onRequest.template;
+          }
+          L.has('debug') && L.log('debug', T.add(logArgs).toMessage({
+            text: tmpl
+          }));
+        }
+        let onSuccess = texture.logging.onSuccess;
+        if (isEnabled(onSuccess)) {
+        }
+        let onFailure = texture.logging.onFailure;
+        if (isEnabled(onFailure)) {
+        }
+      }
+      // detecting for methodType: promise-returned, callback-based or general method
+      let result = undefined;
+      switch(texture.methodType) {
+        case 'promise': {
+          result = Promise.resolve().then(function() {
+            return method.apply(object, parameters)
+          })
+          .then(function(value) {
+            return value;
+          })
+          .catch(function(error) {
+            return Promise.reject(error);
+          })
+          break;
+        }
+        case 'callback': {
+          let withoutCallback = omitCallback(parameters);
+          let methodInPromise = Promise.promisify(method, { context: object });
+          result = methodInPromise.apply(null, withoutCallback)
+          .then(function(value) {
+            return value;
+          })
+          .catch(function(error) {
+            return Promise.reject(error);
+          })
+          .asCallback(pickCallback(parameters));
+          break;
+        }
+        default: {
+          try {
+            result = method.apply(object, parameters);
+          } catch (exception) {
+            throw exception;
+          }
+          break;
+        }
+      }
+      return result;
+    }
+  }
+  return wrapped;
+}
+
+function isDecorated(texture) {
+  return texture && texture.enabled !== false && (
+      (texture.logging && texture.logging.enabled !== false) ||
+      (texture.mocking && texture.mocking.enabled !== false)
+  );
+}
+
+function isEnabled(section) {
+  return section && section.enabled !== false;
+}
+
+function pickCallback(parameters) {
+  return (parameters.length > 0) && parameters[parameters.length - 1];
+}
+
+function omitCallback(parameters) {
+  return Array.prototype.slice.call(parameters, 0, parameters.length);
+}
+
+function getTextureDef({textureStore, pluginCode, gadgetType, objectName, methodName}) {
+  let patternPath = [];
+  if (pluginCode) {
+    if (chores.isSpecialPlugin(pluginCode)) {
+      patternPath.push(pluginCode);
+    } else {
+      patternPath.push('plugins', pluginCode);
+    }
+    if (['services', 'triggers', 'internal'].indexOf(gadgetType) >= 0) {
+      patternPath.push(gadgetType);
+      if (objectName) {
+        patternPath.push(objectName);
+        if (methodName) {
+          patternPath.push(methodName);
+        }
+      }
+    }
+  }
+  return lodash.get(textureStore, patternPath, {});
+}
--- test/tdd/object-decorator-test.js
+++ test/tdd/object-decorator-test.js
@@ -2,15 +2,18 @@
 
 var lab = require('../index');
 var Devebot = lab.getDevebot();
+var Promise = Devebot.require('bluebird');
 var lodash = Devebot.require('lodash');
 var chores = Devebot.require('chores');
 var debugx = Devebot.require('pinbug')('tdd:devebot:core:object-decorator');
 var assert = require('chai').assert;
 var path = require('path');
 var util = require('util');
+var LogAdapter = require('logolite').LogAdapter;
 var LogConfig = require('logolite').LogConfig;
 var LogTracer = require('logolite').LogTracer;
 var envmask = require('envmask').instance;
+var rewire = require('rewire');
 var sinon = require('sinon');
 
 describe('tdd:devebot:core:object-decorator', function() {
@@ -19,28 +22,282 @@ describe('tdd:devebot:core:object-decorator', function() {
   before(function() {
     envmask.setup({
       LOGOLITE_ALWAYS_ENABLED: 'all',
-      LOGOLITE_ALWAYS_MUTED: 'all'
+      // LOGOLITE_ALWAYS_MUTED: 'all'
     });
     LogConfig.reset();
   });
 
   describe('wrapMethod()', function() {
-    it('should wrap a method without textures correctly', function() {
-      var objectDecorator = lab.createObjectDecorator('fullapp', {
-        textureNames: ['default'],
-        textureConfig: {}
-      });
+    var ObjectDecorator = rewire(lab.getDevebotModule('backbone/object-decorator'));
+    var wrapMethod = ObjectDecorator.__get__('wrapMethod');
+
+    it('should wrap a method without texture correctly', function() {
       var originalMethod = sinon.stub();
-      var wrappedMethod = objectDecorator.wrapMethod(originalMethod);
+      var wrappedMethod = wrapMethod({}, originalMethod);
       var result = wrappedMethod({ msg: 'Hello world' }, { reqId: LogConfig.getLogID() });
       assert.equal(originalMethod.callCount, 1);
       assert.equal(originalMethod.firstCall.args.length, 2);
       assert.deepEqual(originalMethod.firstCall.args[0], { msg: 'Hello world' });
       assert.hasAllKeys(originalMethod.firstCall.args[1], ['reqId']);
     });
-  })
 
-  describe('wrapMethodsOf()', function() {
+    it('should wrap logging for a general method correctly', function() {
+      var context = {
+        L: LogAdapter.getLogger(),
+        T: LogTracer.ROOT
+      }
+      var object = {
+        sampleMethod: sinon.stub().callsFake(function() {
+          return {
+            description: "Echo message",
+            parameters: arguments[0]
+          };
+        })
+      }
+      var texture = {
+        methodType: 'general',
+        logging: {
+          onRequest: {
+            extractReqId: function(args, context) {
+              return args && args[1] && args[1].reqId;
+            },
+            extractInfo: function(args, context) {
+              return args[0];
+            },
+            template: "#{objectName}.#{methodName} - #{parameters} - Request[#{requestId}]"
+          },
+          onSuccess: {
+            extractInfo: function(result) {
+              return result;
+            },
+            template: "#{objectName}.#{methodName} - #{output} - Request[#{requestId}]"
+          },
+          onFailure: {
+            extractInfo: function(error) {
+              return {
+                error_code: error.code,
+                error_message: error.message
+              }
+            },
+            template: "#{objectName}.#{methodName} - #{output} - Request[#{requestId}]"
+          }
+        }
+      }
+      var wrappedMethod = wrapMethod(context, object.sampleMethod, texture, {
+        object: object,
+        objectName: 'sampleObject',
+        methodName: 'sampleMethod'
+      });
+      var output = wrappedMethod({ msg: 'Welcome to devebot' }, { reqId: LogConfig.getLogID() });
+      assert.equal(object.sampleMethod.callCount, 1);
+      assert.equal(object.sampleMethod.firstCall.args.length, 2);
+      assert.deepEqual(object.sampleMethod.firstCall.args[0], { msg: 'Welcome to devebot' });
+      assert.hasAllKeys(object.sampleMethod.firstCall.args[1], ['reqId']);
+    });
+
+    it('should wrap logging for a promise method correctly', function(done) {
+      var context = {
+        L: LogAdapter.getLogger(),
+        T: LogTracer.ROOT
+      }
+      var object = {
+        sampleMethod: sinon.stub().callsFake(function() {
+          return Promise.resolve(arguments[0]);
+        })
+      }
+      var texture = {
+        methodType: 'promise',
+        logging: {
+          enabled: true,
+          onRequest: {
+            enabled: true,
+            extractReqId: function(args, context) {
+              return args && args[1] && args[1].reqId;
+            },
+            extractInfo: function(args, context) {
+              return args[0];
+            },
+            template: "#{objectName}.#{methodName} - #{parameters} - Request[#{requestId}]"
+          },
+          onSuccess: {
+            extractInfo: function(result) {
+              return result;
+            },
+            template: "#{objectName}.#{methodName} - #{output} - Request[#{requestId}]"
+          },
+          onFailure: {
+            extractInfo: function(error) {
+              return {
+                error_code: error.code,
+                error_message: error.message
+              }
+            },
+            template: "#{objectName}.#{methodName} - #{output} - Request[#{requestId}]"
+          }
+        }
+      };
+      var wrappedMethod = wrapMethod(context, object.sampleMethod, texture, {
+        object: object,
+        objectName: 'sampleObject',
+        methodName: 'sampleMethod'
+      });
+      var p = wrappedMethod({ msg: 'Welcome to devebot' }, { reqId: LogConfig.getLogID() });
+      p = p.then(function(result) {
+        assert.equal(object.sampleMethod.callCount, 1);
+        assert.equal(object.sampleMethod.firstCall.args.length, 2);
+        assert.deepEqual(object.sampleMethod.firstCall.args[0], { msg: 'Welcome to devebot' });
+        assert.hasAllKeys(object.sampleMethod.firstCall.args[1], ['reqId']);
+        return null;
+      })
+      p.asCallback(done);
+    });
+
+    it('should wrap logging for a callback method correctly', function(done) {
+      var texture = {
+        methodType: 'callback',
+        logging: {
+          enabled: true,
+          onRequest: {
+            enabled: true,
+            extractReqId: function(args, context) {
+              return args && args[1] && args[1].reqId;
+            },
+            extractInfo: function(args, context) {
+              return args[0];
+            },
+            template: "#{objectName}.#{methodName} - #{parameters} - Request[#{requestId}]"
+          },
+          onSuccess: {
+            extractInfo: function(result) {
+              return result;
+            },
+            template: "#{objectName}.#{methodName} - #{output} - Request[#{requestId}]"
+          },
+          onFailure: {
+            extractInfo: function(error) {
+              return {
+                error_code: error.code,
+                error_message: error.message
+              }
+            },
+            template: "#{objectName}.#{methodName} - #{output} - Request[#{requestId}]"
+          }
+        }
+      };
+      var object = {
+        sampleMethod: sinon.stub().callsFake(function() {
+          let cb = arguments[arguments.length - 1];
+          cb(null, { msg: "This is a normal result" });
+        })
+      }
+      var wrappedMethod = wrapMethod({
+        L: LogAdapter.getLogger(),
+        T: LogTracer.ROOT
+      }, object.sampleMethod, texture, {
+        object: object,
+        objectName: 'sampleObject',
+        methodName: 'sampleMethod'
+      });
+      wrappedMethod({ msg: 'Welcome to devebot' }, { reqId: LogConfig.getLogID() }, function(err, value) {
+        done(err, value);
+      });
+    });
+  });
+
+  describe('wrapObject()', function() {
+    var ObjectDecorator = rewire(lab.getDevebotModule('backbone/object-decorator'));
+    var wrapObject = ObjectDecorator.__get__('wrapObject');
+
+    it('should wrap all of public methods of a bean with empty textureStore', function() {
+      var context = {};
+      var textureStore = {};
+      var mockedBean = {
+        method1: sinon.stub(),
+        method2: sinon.stub()
+      }
+      var originalBean = lodash.clone(mockedBean);
+      var wrappedBean = wrapObject(context, textureStore, originalBean, {
+        objectName: 'originalBean'
+      });
+      // invoke method1() 3 times
+      lodash.range(3).forEach(function() {
+        wrappedBean.method1();
+      })
+      assert.equal(mockedBean.method1.callCount, 3);
+      // invoke method1() 2 times
+      lodash.range(2).forEach(function() {
+        wrappedBean.method2();
+      })
+      assert.equal(mockedBean.method2.callCount, 2);
+    });
+
+    it('should wrap all of public methods of a bean', function() {
+      var pluginCTX = {
+        L: LogAdapter.getLogger(),
+        T: LogTracer.ROOT,
+        moduleType: 'plugin'
+      }
+      var textureStore = {
+        plugins: {
+          "simple-plugin": {
+            services: {
+              originalBean: {
+                method1: {
+                  methodType: 'normal', // promise, callback, normal
+                  logging: {
+                    enabled: true,
+                    onRequest: {
+                      enabled: true,
+                      extractReqId: function(args, context) {
+                        return args && args[1] && args[1].reqId;
+                      },
+                      extractInfo: function(args, context) {
+                        return args[0];
+                      },
+                      template: "#{objectName}.#{methodName} - #{parameters} - Request[#{requestId}]"
+                    },
+                    onSuccess: {
+                      extractInfo: function(result) {
+                        return result;
+                      },
+                      template: "#{objectName}.#{methodName} - #{output} - Request[#{requestId}]"
+                    },
+                    onFailure: {
+                      extractInfo: function(error) {
+                        return {
+                          error_code: error.code,
+                          error_message: error.message
+                        }
+                      },
+                      template: "#{objectName}.#{methodName} - #{output} - Request[#{requestId}]"
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      var mockedBean = {
+        method1: sinon.stub(),
+        method2: sinon.stub()
+      }
+      var originalBean = lodash.clone(mockedBean);
+      var wrappedBean = wrapObject(pluginCTX, textureStore, originalBean, {
+        pluginCode: 'simple-plugin',
+        gadgetType: 'services',
+        objectName: 'originalBean'
+      });
+      lodash.range(3).forEach(function() {
+        wrappedBean.method1('Hello world', {
+          reqId: LogConfig.getLogID()
+        });
+      });
+      assert.equal(mockedBean.method1.callCount, 3);
+    });
+  });
+
+  describe('wrapPluginGadget()', function() {
     it('should wrap all of public methods of a bean', function() {
       var objectDecorator = lab.createObjectDecorator('fullapp', {
         textureNames: ['default'],
@@ -52,7 +309,7 @@ describe('tdd:devebot:core:object-decorator', function() {
         method2: sinon.stub()
       }
       
-      var wrappedBean = objectDecorator.wrapMethodsOf(lodash.clone(originalBean), {
+      var wrappedBean = objectDecorator.wrapPluginGadget(lodash.clone(originalBean), {
         objectName: 'originalBean'
       });
 
