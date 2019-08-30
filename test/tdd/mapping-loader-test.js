a9fdd1552cb97a58af6fcbd6c5366b1f4d854f47
pnhung177 <pnhung177@acegik.net>
2019-08-30 09:48:54 +0700

    Move the testcases of traverseDirXYZ functions from the app-restfetch plugin

--- test/tdd/mapping-loader-test.js
+++ test/tdd/mapping-loader-test.js
@@ -3,7 +3,10 @@
 var lab = require('../index');
 var Devebot = lab.getDevebot();
 var chores = Devebot.require('chores');
-var assert = require('chai').assert;
+var assert = require('liberica').assert;
+var mockit = require('liberica').mockit;
+var sinon = require('liberica').sinon;
+var path = require('path');
 
 describe('tdd:devebot:core:mapping-loader', function() {
   var loggingFactory = lab.createLoggingFactoryMock();
@@ -11,6 +14,238 @@ describe('tdd:devebot:core:mapping-loader', function() {
     L: loggingFactory.getLogger(),
     T: loggingFactory.getTracer(),
   }
+
+  describe('traverseDir()', function() {
+    var MappingLoader, traverseDir, traverseDirRecursively;
+
+    beforeEach(function() {
+      MappingLoader = mockit.acquire('mapping-loader', {
+        moduleHome: path.join(__dirname, '../../lib/backbone')
+      });
+      traverseDir = mockit.get(MappingLoader, 'traverseDir');
+      traverseDirRecursively = mockit.spy(MappingLoader, 'traverseDirRecursively');
+
+      assert.isFunction(traverseDir);
+      assert.isFunction(traverseDirRecursively);
+    });
+
+    it('should standardize the directory path', function() {
+      let args;
+
+      traverseDir("", [".js"]);
+      args = traverseDirRecursively.getCall(0).args;
+      assert.equal(args[0], ".");
+      assert.equal(args[1], ".");
+      traverseDirRecursively.resetHistory();
+
+      traverseDir(path.sep, [".js"]);
+      args = traverseDirRecursively.getCall(0).args;
+      assert.equal(args[0], path.sep);
+      assert.equal(args[1], path.sep);
+      traverseDirRecursively.resetHistory();
+
+      const MAPPING_DIR = ["", "home", "devebot", "example"].join(path.sep);
+
+      traverseDir(MAPPING_DIR, [".js"]);
+      args = traverseDirRecursively.getCall(0).args;
+      assert.equal(args[0], MAPPING_DIR);
+      assert.equal(args[1], MAPPING_DIR);
+      traverseDirRecursively.resetHistory();
+
+      traverseDir(MAPPING_DIR + path.sep, [".js"]);
+      args = traverseDirRecursively.getCall(0).args;
+      assert.equal(args[0], MAPPING_DIR);
+      assert.equal(args[1], MAPPING_DIR);
+      traverseDirRecursively.resetHistory();
+    });
+
+    const MAPPING_DIR = ["", "home", "devebot", "example"].join(path.sep);
+    const RELATIVE_DIR = ["", "mappings"].join(path.sep);
+
+    it('should match filenames with a RegExp', function() {
+      let args;
+
+      traverseDir(MAPPING_DIR, /\.js$/);
+      args = traverseDirRecursively.getCall(0).args;
+      const filter = args[2];
+      // make sure the "filter" is a function
+      assert.isFunction(filter);
+      // assert that "filter" satisfied the provided regular expression
+      // case 1: { path: "/mappings", base: "github-api.js" }
+      assert.isTrue(filter({ path: RELATIVE_DIR, base: "github-api.js" }));
+      assert.isFalse(filter({ path: RELATIVE_DIR, base: "github-api.md" }));
+      assert.isFalse(filter({ path: RELATIVE_DIR, base: "github.jsi.md" }));
+      assert.isFalse(filter({ path: RELATIVE_DIR, base: "github-api.jsx" }));
+      assert.isFalse(filter({ path: RELATIVE_DIR, base: "github-api_js" }));
+      assert.isFalse(filter({ path: ["", ".jszz.js"].join(path.sep), base: "github-api.md" }));
+      traverseDirRecursively.resetHistory();
+    });
+
+    it('should match filenames with an array of extensions', function() {
+      let args;
+
+      traverseDir(MAPPING_DIR, ["jsi", "jsx", "zz","JAR","json","html"]);
+      args = traverseDirRecursively.getCall(0).args;
+      const filter = args[2];
+
+      assert.isFalse(filter({ path: RELATIVE_DIR, base: "github-api.js" }));
+      assert.isFalse(filter({ path: RELATIVE_DIR, base: "github-api.md" }));
+      assert.isTrue(filter({ path: RELATIVE_DIR, base: "github.jsi.md" }));
+      assert.isTrue(filter({ path: RELATIVE_DIR, base: "github-api.jsx" }));
+      assert.isFalse(filter({ path: RELATIVE_DIR, base: "github-api_js" }));
+      assert.isTrue(filter({ path: RELATIVE_DIR, base: "github-api.json" }));
+      assert.isTrue(filter({ path: RELATIVE_DIR, base: "github-api.JAR" }));
+      assert.isFalse(filter({ path: RELATIVE_DIR, base: "github-api.exe" }));
+      assert.isTrue(filter({ path: RELATIVE_DIR, base: "github.json.exe" }));
+      assert.isFalse(filter({ path: RELATIVE_DIR, base: "github.png.exe" }));
+      assert.isFalse(filter({ path: RELATIVE_DIR, base: "github.txt.exe" }));
+      assert.isTrue(filter({ path: RELATIVE_DIR, base: "github.html.jsx" }));
+
+      // { path: "/.jszz.js", base: "github-api.md" } => relative-path: /.jszz.js/github-api.md
+      assert.isTrue(filter({ path: ["", ".txtmd.html"].join(path.sep), base: "github-api.png" }));
+      assert.isFalse(filter({ path: ["", ".png.exe"].join(path.sep), base: "github-api.js" }));
+      assert.isTrue(filter({ path: ["", ".txtzz.js"].join(path.sep), base: "github-api.jsx" }));
+      assert.isTrue(filter({ path: ["", ".JARpng.js"].join(path.sep), base: "github-api.txt" }));
+      assert.isFalse(filter({ path: ["", ".exemd.js"].join(path.sep), base: "github-api.md" }));
+      traverseDirRecursively.resetHistory();
+    });
+
+    it('should match filenames with a string', function() {
+      let args;
+
+      traverseDir(MAPPING_DIR, "mappings" + path.sep +"github");
+      args = traverseDirRecursively.getCall(0).args;
+      const filter = args[2];
+
+      assert.isFalse(filter({ path: RELATIVE_DIR, base: "gitlab-api.js" }));
+      assert.isTrue(filter({ path: RELATIVE_DIR, base: "github-api.md" }));
+      assert.isTrue(filter({ path: RELATIVE_DIR, base: "github.jsi.md" }));
+      assert.isFalse(filter({ path: RELATIVE_DIR, base: "gitlab.jsi.zz" }));
+      assert.isFalse(filter({ path: RELATIVE_DIR, base: "gitbranch-api.txt" }));
+      assert.isFalse(filter({ path: ["", ".pngexe.js"].join(path.sep), base: "gitlab-api.txt" }));
+      assert.isFalse(filter({ path: ["", ".jsxzz.js"].join(path.sep), base: "gitbranch-api.zz" }));
+      traverseDirRecursively.resetHistory();
+    });
+  });
+
+  describe('traverseDirRecursively()', function() {
+    var loggingFactory = mockit.createLoggingFactoryMock({ captureMethodCall: false });
+    var ctx = {
+      L: loggingFactory.getLogger(),
+      T: loggingFactory.getTracer(),
+      blockRef: 'app-restfetch',
+    }
+
+    const MAPPING_HOME_DIR = ["", "home", "devebot", "example", "mappings"].join(path.sep);
+    const statOfDirectory = {
+      isDirectory: function() { return true },
+      isFile: function() { return false },
+    }
+    const statOfFile = {
+      isDirectory: function() { return false },
+      isFile: function() { return true },
+    }
+
+    function mappingFileFilter(fileinfo) {
+      return ['.js'].indexOf(fileinfo.ext) >= 0;
+    }
+
+    var MappingLoader, traverseDirRecursively, fs;
+
+    beforeEach(function() {
+      MappingLoader = mockit.acquire('mapping-loader', {
+        moduleHome: path.join(__dirname, '../../lib/backbone')
+      });
+      traverseDirRecursively = mockit.get(MappingLoader, 'traverseDirRecursively');
+      fs = mockit.stubObject(MappingLoader, 'fs', ['readdirSync', 'statSync']);
+    });
+
+    it('get all of names of filtered files in a directory', function() {
+      fs.readdirSync.withArgs(MAPPING_HOME_DIR)
+        .returns([
+          "github-api.js",
+          "gitlab-api.js",
+          "readme.md"
+        ])
+      fs.statSync.withArgs(path.join(MAPPING_HOME_DIR, "github-api.js")).returns(statOfFile)
+      fs.statSync.withArgs(path.join(MAPPING_HOME_DIR, "gitlab-api.js")).returns(statOfFile)
+      fs.statSync.withArgs(path.join(MAPPING_HOME_DIR, "readme.md")).returns(statOfFile);
+      assert.deepEqual(traverseDirRecursively(MAPPING_HOME_DIR, MAPPING_HOME_DIR, mappingFileFilter), [
+        {
+          "home": MAPPING_HOME_DIR,
+          "path": "",
+          "dir": MAPPING_HOME_DIR,
+          "base": "github-api.js",
+          "name": "github-api",
+          "ext": ".js"
+        },
+        {
+          "home": MAPPING_HOME_DIR,
+          "path": "",
+          "dir": MAPPING_HOME_DIR,
+          "base": "gitlab-api.js",
+          "name": "gitlab-api",
+          "ext": ".js"
+        }
+      ]);
+    });
+
+    it('get all of names of recursive filtered files in a directory', function() {
+      fs.readdirSync.withArgs(MAPPING_HOME_DIR).returns([
+        "api",
+        "vcs",
+        "doc",
+        "index.js",
+        "readme.md"
+      ]);
+      fs.statSync.withArgs(path.join(MAPPING_HOME_DIR, "api")).returns(statOfDirectory);
+      fs.statSync.withArgs(path.join(MAPPING_HOME_DIR, "vcs")).returns(statOfDirectory);
+      fs.statSync.withArgs(path.join(MAPPING_HOME_DIR, "doc")).returns(statOfDirectory);
+      fs.statSync.withArgs(path.join(MAPPING_HOME_DIR, "index.js")).returns(statOfFile)
+      fs.statSync.withArgs(path.join(MAPPING_HOME_DIR, "readme.md")).returns(statOfFile);
+
+      fs.readdirSync.withArgs(path.join(MAPPING_HOME_DIR, "vcs")).returns([
+        "git"
+      ]);
+      fs.statSync.withArgs(path.join(MAPPING_HOME_DIR, "vcs", "git")).returns(statOfDirectory)
+
+      fs.readdirSync.withArgs(path.join(MAPPING_HOME_DIR, "vcs", "git")).returns([
+        "github-api.js",
+        "gitlab-api.js",
+        "readme.md"
+      ]);
+      fs.statSync.withArgs(path.join(MAPPING_HOME_DIR, "vcs", "git", "github-api.js")).returns(statOfFile)
+      fs.statSync.withArgs(path.join(MAPPING_HOME_DIR, "vcs", "git", "gitlab-api.js")).returns(statOfFile)
+      fs.statSync.withArgs(path.join(MAPPING_HOME_DIR, "vcs", "git", "readme.md")).returns(statOfFile);
+
+      assert.deepEqual(traverseDirRecursively(MAPPING_HOME_DIR, MAPPING_HOME_DIR, mappingFileFilter), [
+        {
+          "home": MAPPING_HOME_DIR,
+          "path": path.join(path.sep, "vcs", "git"),
+          "dir": path.join(MAPPING_HOME_DIR, "vcs", "git"),
+          "base": "github-api.js",
+          "name": "github-api",
+          "ext": ".js"
+        },
+        {
+          "home": MAPPING_HOME_DIR,
+          "path": path.join(path.sep, "vcs", "git"),
+          "dir": path.join(MAPPING_HOME_DIR, "vcs", "git"),
+          "base": "gitlab-api.js",
+          "name": "gitlab-api",
+          "ext": ".js"
+        },
+        {
+          "home": MAPPING_HOME_DIR,
+          "path": "",
+          "dir": MAPPING_HOME_DIR,
+          "base": "index.js",
+          "name": "index",
+          "ext": ".js"
+        }
+      ]);
+    });
+  });
 
   describe('loadMappingStore()', function() {
     var loggingFactory = lab.createLoggingFactoryMock({ captureMethodCall: false });
