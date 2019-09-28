1ae086bc8e069ff8042c805adec38f8188c73167
pnhung177 <pnhung177@acegik.net>
2019-09-28 21:56:16 +0700

    Add the envcfg utility that is used to extract the configuration values from environment variables

---
+++ test/tdd/utils-envcfg-test.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var lab = require('../index');
+var assert = require('chai').assert;
+var envcloak = require('envcloak').instance;
+var envcfg = require(lab.getDevebotModule('utils/envcfg'));
+
+describe('tdd:devebot:utils:envcfg', function() {
+  describe('extractEnv()', function() {
+    before(function() {
+      envcloak.setup({
+        A_PREFIX_OF_ENVCFG_plugins_example_settings_supportContacts_phoneNumber: "+84987654321",
+        A_PREFIX_OF_ENVCFG_plugins_example_settings_supportContacts_email: "contact@devebot.com",
+        NODE_ENV: 'test'
+      });
+    });
+
+    it('filters and extracts configuration values properly', function() {
+      const cfgMap = envcfg.extractEnv('A_PREFIX_OF_ENVCFG_');
+      false && console.log('Configuration: %s', JSON.stringify(cfgMap, null, 2));
+      assert.deepEqual(cfgMap, {
+        "plugins": {
+          "example": {
+            "settings": {
+              "supportContacts": {
+                "phoneNumber": "+84987654321",
+                "email": "contact@devebot.com"
+              }
+            }
+          }
+        }
+      });
+    });
+
+    it('return empty result when none of environment variables satisfied the prefix', function() {
+      assert.deepEqual(envcfg.extractEnv('NOT_FOUND_PREFIX_ENVCFG'), {});
+    });
+
+    after(function() {
+      envcloak.reset();
+    });
+  })
+});
