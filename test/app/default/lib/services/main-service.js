b7bd2d3eb17dde76a84404a3c266a7fc4691f39b
pnhung177 <pnhung177@gmail.com>
2023-01-26 11:31:48 +0700

    Sanitizes the example 'default'

--- test/app/default/lib/services/main-service.js
+++ test/app/default/lib/services/main-service.js
@@ -1,13 +1,27 @@
 /* global Devebot */
 "use strict";
 
+const chores = Devebot.require("chores");
 const lodash = Devebot.require("lodash");
 
-var Service = function(params={}) {
-  const L = params.loggingFactory.getLogger();
+function Service (params={}) {
+  const sandboxConfig = lodash.get(params, ["sandboxConfig", "application"], {});
 
-  const mainCfg = lodash.get(params, ["sandboxConfig", "application"], {});
-  L.has("silly") && L.log("silly", "configuration: %s", JSON.stringify(mainCfg));
+  const { loggingFactory, packageName } = params;
+
+  const L = loggingFactory.getLogger();
+  const T = loggingFactory.getTracer();
+  const blockRef = chores.getBlockRef(__filename, packageName);
+
+  this.getConfig = function () {
+    return {
+      [blockRef]: sandboxConfig,
+    };
+  };
+  L && L.has("debug") && L.log("debug", T && T.add({ blockRef }).toMessage({
+    tags: [ blockRef, "configuration" ],
+    text: "Configuration: " + JSON.stringify(this.getConfig())
+  }));
 };
 
 module.exports = Service;
--- test/app/default/lib/triggers/main-trigger.js
+++ test/app/default/lib/triggers/main-trigger.js
@@ -5,31 +5,37 @@ const http = require("http");
 const Promise = Devebot.require("bluebird");
 const lodash = Devebot.require("lodash");
 const chores = Devebot.require("chores");
-const debugx = Devebot.require("pinbug")("devebot:test:lab:main:mainTrigger");
 
-const Service = function(params={}) {
-  const packageName = params.packageName || "demo-app";
-  const mainCfg = lodash.get(params, ["sandboxConfig", "application"], {});
+function Service (params={}) {
+  const sandboxConfig = lodash.get(params, ["sandboxConfig", "application"], {});
+  const { packageName, loggingFactory } = params;
+
+  const L = loggingFactory && loggingFactory.getLogger();
+  const T = loggingFactory && loggingFactory.getTracer();
+  const blockRef = chores.getBlockRef(__filename, packageName);
 
   const server = http.createServer();
 
   server.on("error", function(err) {
-    debugx.enabled && debugx("Server Error: %s", JSON.stringify(err));
+    L && L.has("silly") && L.log("silly", T && T.add({ blockRef }).toMessage({
+      tags: [ blockRef, "http-server-error" ],
+      text: "Server error: " + JSON.stringify(err)
+    }));
   });
 
   this.getServer = function() {
     return server;
   };
 
-  const configHost = lodash.get(mainCfg, "host", "0.0.0.0");
-  const configPort = lodash.get(mainCfg, "port", 8080);
+  const configHost = lodash.get(sandboxConfig, "host", "0.0.0.0");
+  const configPort = lodash.get(sandboxConfig, "port", 8080);
 
   this.start = function() {
     return new Promise(function(resolve, reject) {
       const serverInstance = server.listen(configPort, configHost, function () {
         const host = serverInstance.address().address;
         const port = serverInstance.address().port;
-        chores.isVerboseForced(packageName, mainCfg) &&
+        chores.isVerboseForced(packageName, sandboxConfig) &&
         chores.logConsole("%s is listening at http://%s:%s", packageName, host, port);
         resolve(serverInstance);
       });
@@ -39,7 +45,7 @@ const Service = function(params={}) {
   this.stop = function() {
     return new Promise(function(resolve, reject) {
       server.close(function () {
-        chores.isVerboseForced(packageName, mainCfg) &&
+        chores.isVerboseForced(packageName, sandboxConfig) &&
         chores.logConsole("%s has been closed", packageName);
         resolve();
       });
