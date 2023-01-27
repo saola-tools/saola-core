f41adacd7d57b71fe71d4ffcd0e8ad43c1a17818
pnhung177 <pnhung177@gmail.com>
2023-01-27 13:59:54 +0700

    Sanitizes the package 'devebot-co-adapter'

--- test/lib/devebot-co-adapter/dialect.js
+++ test/lib/devebot-co-adapter/dialect.js
@@ -1,14 +1,14 @@
 /* global Devebot */
-'use strict';
+"use strict";
 
-const lodash = Devebot.require('lodash');
+const lodash = Devebot.require("lodash");
 
 function Dialect (params = {}) {
   this.getConfig = function() {
     return lodash.cloneDeep(params);
-  }
+  };
 };
 
-Dialect.manifest = require('./manifest');
+Dialect.manifest = require("./manifest");
 
 module.exports = Dialect;
