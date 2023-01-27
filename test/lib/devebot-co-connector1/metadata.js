6444e1ed161c6b2086e057d971115ea831d4e39c
pnhung177 <pnhung177@gmail.com>
2023-01-27 14:02:27 +0700

    Sanitizes the packages 'devebot-co-connector1' and 'devebot-co-connector2'

--- test/lib/devebot-co-connector1/metadata.js
+++ test/lib/devebot-co-connector1/metadata.js
@@ -14,4 +14,4 @@ module.exports = {
     },
     "required": ["refName", "refType"]
   }
-}
+};
--- test/lib/devebot-co-connector2/bridge.js
+++ test/lib/devebot-co-connector2/bridge.js
@@ -1,23 +1,23 @@
 /* global Devebot */
-'use strict';
+"use strict";
 
-const lodash = Devebot.require('lodash');
-const devlog = Devebot.require('pinbug')('devebot:test:lab:devebot-co-connector2');
+const lodash = Devebot.require("lodash");
+const devlog = Devebot.require("pinbug")("devebot:test:lab:devebot-co-connector2");
 
 const Service = function(params) {
-  devlog.enabled && devlog(' + constructor start ...');
+  devlog.enabled && devlog(" + constructor start ...");
 
   params = params || {};
 
-  devlog.enabled && devlog(' - params: %s', JSON.stringify(params, null, 2));
+  devlog.enabled && devlog(" - params: %s", JSON.stringify(params, null, 2));
 
   this.getConfig = function() {
     return lodash.cloneDeep(params);
-  }
+  };
 
-  devlog.enabled && devlog(' - constructor end!');
+  devlog.enabled && devlog(" - constructor end!");
 };
 
-Service.metadata = require('./metadata');
+Service.metadata = require("./metadata");
 
 module.exports = Service;
